5541055c8678fb4f287595796923cefcb98ad7f0
nycocado <nycolascanutto@gmail.com>
2024-10-30 17:24:46 +0000

    test change name of the branch

--- app/app/src/main/java/pt/iade/ei/thinktoilet/MainActivity.kt
+++ app/app/src/main/java/pt/iade/ei/thinktoilet/MainActivity.kt
@@ -4,22 +4,19 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxHeight
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.verticalScroll
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import pt.iade.ei.thinktoilet.models.Position
-import pt.iade.ei.thinktoilet.models.RatingCategory
-import pt.iade.ei.thinktoilet.models.Toilet
+import androidx.compose.ui.unit.dp
+import pt.iade.ei.thinktoilet.test.generateToilet
 import pt.iade.ei.thinktoilet.ui.components.LocationCard
 import pt.iade.ei.thinktoilet.ui.theme.ThinkToiletTheme
 
@@ -38,7 +35,9 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun MainView() {
     ThinkToiletTheme {
-        Scaffold {}
+        Scaffold { innerPadding ->
+            
+        }
     }
 }
 
--- app/app/src/main/java/pt/iade/ei/thinktoilet/ui/components/LocationCard.kt
+++ app/app/src/main/java/pt/iade/ei/thinktoilet/ui/components/LocationCard.kt
@@ -2,17 +2,17 @@ package pt.iade.ei.thinktoilet.ui.components
 
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextOverflow
@@ -24,12 +24,23 @@ import pt.iade.ei.thinktoilet.ui.theme.montserratFontFamily
 
 @Composable
 fun LocationCard(
-    toilet: Toilet
+    toilet: Toilet,
+    distance: String
 ) {
-    Box(
+    Card(
         modifier = Modifier
-            .clip(RoundedCornerShape(8.dp))
-            .border(3.dp, Color.LightGray, RoundedCornerShape(15.dp))
+            .fillMaxWidth()
+            .padding(all = 6.dp)
+            .border(
+                width = 2.dp,
+                color = Color.LightGray,
+                shape = MaterialTheme.shapes.medium
+            ),
+        shape = MaterialTheme.shapes.medium,
+        colors = CardDefaults.cardColors(
+            containerColor = Color.White,
+        ),
+        elevation = CardDefaults.cardElevation(4.dp)
     ) { // Container (Serve para aplicar o border radius)
         Row(
             modifier = Modifier
@@ -48,7 +59,7 @@ fun LocationCard(
                         fontFamily = montserratFontFamily,
                         fontWeight = FontWeight.SemiBold,
                         maxLines = 1,
-                        overflow = TextOverflow.Ellipsis
+                        overflow = TextOverflow.Ellipsis,
                     )
                 }
                 Row(
@@ -76,7 +87,7 @@ fun LocationCard(
             Column { // Distância
                 Text(
                     modifier = Modifier.padding(horizontal = 10.dp),
-                    text = "800m",
+                    text = distance,
                     fontFamily = montserratFontFamily,
                     fontWeight = FontWeight.SemiBold,
                     maxLines = 1
@@ -90,6 +101,7 @@ fun LocationCard(
 @Composable
 fun LocationCardPreview() {
     LocationCard(
-        toilet = generateToilet()
+        toilet = generateToilet(),
+        distance = "1.2 km"
     )
 }
