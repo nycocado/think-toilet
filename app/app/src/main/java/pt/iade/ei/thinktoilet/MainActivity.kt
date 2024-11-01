9d1f189e246b4612c33a212b40a19d017a370bbe
nycocado <nycolascanutto@gmail.com>
2024-11-01 01:29:54 +0000

    Added BottomSheet

--- app/app/src/main/java/pt/iade/ei/thinktoilet/MainActivity.kt
+++ app/app/src/main/java/pt/iade/ei/thinktoilet/MainActivity.kt
@@ -5,28 +5,15 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.BottomAppBar
-import androidx.compose.material3.BottomSheetScaffold
-import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBar
-import androidx.compose.material3.rememberBottomSheetScaffoldState
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableIntStateOf
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import pt.iade.ei.thinktoilet.test.generateToilet
-import pt.iade.ei.thinktoilet.ui.components.LocationCard
+import pt.iade.ei.thinktoilet.ui.components.BottomSheetScaffoldThinkToilet
 import pt.iade.ei.thinktoilet.ui.theme.ThinkToiletTheme
 
 class MainActivity : ComponentActivity() {
@@ -41,55 +28,26 @@ class MainActivity : ComponentActivity() {
     }
 }
 
-@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun MainView() {
     ThinkToiletTheme {
-        var sheetState = rememberBottomSheetScaffoldState()
-
         Scaffold(
-            topBar = {
-                TopAppBar(
-                    title = {
-                        Text(text = "ThinkToilet")
-                    }
-                )
-            },
             bottomBar = {
-                BottomAppBar (
-                    content = {
-                        Text(text = "BottomAppBar")
-                    }
-                )
+                BottomAppBar {
+                    Text(
+                        text = "BottomAppBar",
+                        modifier = Modifier.padding(16.dp)
+                    )
+                }
             }
         ) { innerPadding ->
             Box(
-                modifier = Modifier
+                Modifier
                     .padding(innerPadding)
-                    .fillMaxSize()
             ) {
-                BottomSheetScaffold(
-                    sheetContent = {
-                        LazyColumn(
-                            modifier = Modifier.fillMaxSize()
-                        ) {
-                            items(10) {
-                                LocationCard(
-                                    toilet = generateToilet(),
-                                    distance = "1.2 km"
-                                )
-                            }
-                        }
-                    },
-                    sheetPeekHeight = 160.dp,
-                    scaffoldState = sheetState
-                ) {
-
-                }
+                BottomSheetScaffoldThinkToilet()
             }
-
         }
-
     }
 }
 
