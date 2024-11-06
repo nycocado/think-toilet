51b99b26137f49d1b4cd76fdad4bc50f48285bad
nycocado <nycolascanutto@gmail.com>
2024-11-06 11:01:57 +0000

    Added Material Colors

--- app/app/src/main/java/pt/iade/ei/thinktoilet/ui/components/BottomSheetScaffold.kt
+++ app/app/src/main/java/pt/iade/ei/thinktoilet/ui/components/BottomSheetScaffold.kt
@@ -6,10 +6,13 @@ import androidx.compose.foundation.LocalOverscrollConfiguration
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.BottomSheetScaffold
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.SheetValue
+import androidx.compose.material3.Text
 import androidx.compose.material3.rememberBottomSheetScaffoldState
 import androidx.compose.material3.rememberStandardBottomSheetState
 import androidx.compose.runtime.Composable
@@ -92,21 +95,16 @@ fun SelectedToiletContent(toilet: Toilet) {
 @OptIn(ExperimentalFoundationApi::class)
 @Composable
 fun ToiletListContent(toilets: List<Toilet>, onToiletSelected: (Toilet) -> Unit) {
-    CompositionLocalProvider(
-        LocalOverscrollConfiguration provides null
-    ) { // Remove o efeito de overscroll do LazyColumn
-        LazyColumn(
-            modifier = Modifier
-                .fillMaxSize()
-        ) {
-            items(5) {
-                val toilet = toilets[it]
-                LocationCard(
-                    toilet = toilet,
-                    distance = generateRandomDistance(),
-                    onClick = onToiletSelected
-                )
-            }
+    LazyColumn(
+        modifier = Modifier
+            .fillMaxSize()
+    ) {
+        items(toilets) { toilet ->
+            LocationCard(
+                toilet = toilet,
+                distance = generateRandomDistance(),
+                onClick = onToiletSelected
+            )
         }
     }
 }
