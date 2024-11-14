5f72b864188f0242ff7d1b462d4aaa3ba42942b0
Luan Ribeiro <20230692@iade.pt>
2024-11-14 00:44:24 +0000

    Fix merge

--- app/app/src/main/java/pt/iade/ei/thinktoilet/ui/pages/RatingPage.kt
+++ app/app/src/main/java/pt/iade/ei/thinktoilet/ui/pages/RatingPage.kt
@@ -10,6 +10,7 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
@@ -60,7 +61,7 @@ fun RatingPage(
                 fontWeight = FontWeight.Bold
             )
         }
-        RatingUser(user)
+        RatingUser(userMain = userMain)
         RatingComment(commentInput) {
             commentInput = it
         }
